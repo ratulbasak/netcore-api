b123aee9fbf07f65c9f54f8961c78e47ffc91c82
ratulbasak93 <ratulbasak93@gmail.com>
2019-08-01 06:52:15 +0600

    updated jenkinsfile sshput

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,7 +1,7 @@
 # Set-ExecutionPolicy -Scope CurrentUser Unrestricted -Force
 Import-Module Webadministration
 
-$PACKAGE_NAME=$args[0]
+$PACKAGE_NAME = $args[0]
 
 $serviceName = "netcoreapi"
 $DOCDIR = "C:\netcore-api"
@@ -35,7 +35,7 @@ if (!(Test-Path IIS:\Sites\$serviceName -pathType container))
     New-Item IIS:\Sites\$serviceName -bindings @{protocol="http";bindingInformation=":80:" + $serviceName} -physicalPath $extractDestination\$BaseFileName
 }
 
-Set-ItemProperty IIS:\Sites\$serviceName -name  physicalPath -value $extractDestination\$BaseFileName
+Set-ItemProperty IIS:\Sites\$serviceName -name  physicalPath -value "$extractDestination\$BaseFileName"
 echo "Changed $serviceName SourePath in IIS..."
 
 echo "Restarting WebAppPool"
