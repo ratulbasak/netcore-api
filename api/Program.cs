b6f4b366eb813bc72a4e7b0a9d1f2cf6106bb76f
continuous-deployment <continuous.deployment@selise.ch>
2019-08-03 21:42:55 +0600

    updated jenkinsfile

---
+++ api/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace netcore_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+	        .UseUrls("http://0.0.0.0:5000/")
+	        .Build()
+            .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
