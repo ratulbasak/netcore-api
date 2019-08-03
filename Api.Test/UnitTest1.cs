dc82f5f0391c84a30e06f731233d1e2ffd01b380
continuous-deployment <continuous.deployment@selise.ch>
2019-08-03 21:11:03 +0600

    added Api.Test for unit test

---
+++ Api.Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using netcore_api.Controllers;
+
+namespace Api.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new ValuesController();
+            var res = controller.Get(1);
+            Assert.AreEqual(res.Value, "Sunday");
+            Console.Write("Lalu");
+        }
+    }
+}
